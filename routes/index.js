f276a15995620f2a6698172d308ed55bb75906c4
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-10-03 10:36:45 -0700

    Finished basic functionality.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', msg_list: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.msg, user: req.body.name, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
